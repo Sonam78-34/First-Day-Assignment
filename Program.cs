39f5c6fe32e230278553face426ca9b1cebdd9b1
sonam <sonamsinghh919@gmail.com>
2023-04-03 23:10:41 +0530

    Project added

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the String");
+            string prevString = Console.ReadLine();
+            string modifiedString= "";
+
+            for (int i = 0; i < prevString.Length; i++)
+            {
+                char c = prevString[i];
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                    c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                {
+                    continue; // skip vowels
+                }
+                modifiedString += c;
+                
+            }
+
+            Console.WriteLine("Original string: " + prevString);
+            Console.WriteLine("Updated string: " + modifiedString);
+
+
+
+        }
+    }
+}
